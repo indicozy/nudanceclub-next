67d209ca205f64224723220012a0aac60c01a9c1
indicozy <berkutkarlibai@gmail.com>
2023-04-19 03:26:05 +0600

    test: unfinished

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Logo = () => {
   return (
-    <div className="font-runs grid h-16 w-16 place-items-center text-xl leading-none">
-      <Image
-        src={"/logo.png"}
-        width={400}
-        height={400}
-        alt="Nu dance club logo"
-      />
-    </div>
+    <Link href={"/#"}>
+      <div className="font-runs grid h-16 w-16 place-items-center text-xl leading-none">
+        <Image
+          src={"/logo.png"}
+          width={400}
+          height={400}
+          alt="Nu dance club logo"
+        />
+      </div>
+    </Link>
   );
 };
 export default Logo;
